8abe4fe19514d079feba12a6c1bcb32d80f631d8
Lockeriano <patryk.skrzyniarz19@gmail.com>
2017-11-19 14:52:02 +0100

    Stop button working

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 var count = 1500;
+var isStoped = false;
 var startBtn = document.getElementById('start-btn');
+var stopBtn = document.getElementById('stop-btn');
 
+stopBtn.addEventListener('click', changeFlag);
 startBtn.addEventListener('click', timerStart);
 
 
-function timerStart(){
+function timerStart(e){
   var interval = setInterval(function timer() {
-    count = count - 1;
-    if (count == -1) {
+    if (isStoped) {
+      return;
+    } else (
+      count = count - 1
+    )
+    if (count === -1) {
         clearInterval(interval);
         return;
     }
+    isStoped = false;
     var seconds = count % 60;
     var minutes = Math.floor(count / 60);
     minutes %= 60;
 
     document.getElementById("timerContainer").innerHTML = minutes + ":" + seconds;
   }, 1000);
+}
+
+function changeFlag(){
+  if (!isStoped){
+    isStoped = true;
+  } else {
+    isStoped = false;
+  }
 }
