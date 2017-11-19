b20b0ec38a68caa046946a390e30143e3fb534af
Lockeriano <patryk.skrzyniarz19@gmail.com>
2017-11-19 22:12:36 +0100

    Added breaks after pomodoro and long break every 3 pomodoros

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var count = 1500;
+var count = 5;
 var isStoped = false;
+var isBreak = true;
+var i = 1;
 var startBtn = document.getElementById('start-btn');
 var stopBtn = document.getElementById('stop-btn');
 
@@ -7,16 +9,25 @@ stopBtn.addEventListener('click', changeFlag);
 startBtn.addEventListener('click', timerStart);
 
 
-function timerStart(e){
+function timerStart(){
   var interval = setInterval(function timer() {
     if (isStoped) {
       return;
-    } else (
-      count = count - 1
-    )
-    if (count === -1) {
-        clearInterval(interval);
-        return;
+    } else {
+      count = count - 1;
+    }
+    if (count === -1 && isBreak) {
+      count = 3;
+      isBreak = false;
+      i++;
+      console.log(i)
+      if (i % 3 === 0) {
+        count = 12;
+      }
+      return;
+    } else if (count === -1 && !isBreak) {
+      count = 5;
+      isBreak = true;
     }
     isStoped = false;
     var seconds = count % 60;
@@ -27,7 +38,7 @@ function timerStart(e){
   }, 1000);
 }
 
-function changeFlag(){
+function changeFlag() {
   if (!isStoped){
     isStoped = true;
   } else {
